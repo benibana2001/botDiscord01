f554b1d5b205a5120aec933bd9b3180548b2ada6
benibana2001 <benibana2001@gmail.com>
2019-10-11 21:39:45 +0900

    edit

--- discordbot.py
+++ discordbot.py
@@ -1,10 +1,15 @@
-# インストールした discord.py を読み込む
 import discord
+# import setting
 import os
 
 # Set Token from dotenv file
+
+# For debug @local
 # TOKEN = setting.APIKEY01
+
+# For release @Heroku
 TOKEN = os.environ.get('API_KEY_01')
+
 # os.environ.get('CHROME_DRIVER_PATH')
 # Create Client instance
 client = discord.Client()
@@ -22,8 +27,24 @@ async def on_message(message):
     if message.author.bot:
         return
     # 「/neko」と発言したら「にゃーん」
-    if message.content == '/neko':
+    if message.content == '/hana':
         await message.channel.send('にゃーん')
+    # メンバーのリストを取得して表示
+    if message.content == '/members':
+        await message.channel.send(message.guild.members)
+        print(message.guild.members)
+    # 役職のリストを取得して表示
+    if message.content == '/roles':
+        print(message.guild.roles)
+    # テキストチャンネルのリストを取得して表示
+    if message.content == '/text_channels':
+        print(message.guild.text_channels)
+    # ボイスチャンネルのリストを取得して表示
+    if message.content == '/voice_channels':
+        print(message.guild.voice_channels)
+    # カテゴリチャンネルのリストを取得して表示
+    if message.content == '/category_channels':
+        print(message.guild.categories)
 
 # Run
 client.run(TOKEN)
